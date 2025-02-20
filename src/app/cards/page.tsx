1653f8074d405f9df6ece0aea9ca3d94302dab92
Federico Barbieri <federico.barbieri@grupposcai.it>
2025-02-20 09:13:05 +0100

    Redefine folder structure, drizzle and db setup (WIP)

---
+++ src/app/cards/page.tsx
@@ -0,0 +1,11 @@
+import Card from "@/components/card/card";
+
+export default function Home() {
+	return (
+		<main className="mt-header flex flex-col items-center px-base sm:mt-12">
+			<section className="mx-auto flex w-full max-w-xl flex-col items-center sm:flex-row">
+				<p>Sezione WIP</p>
+			</section>
+		</main>
+	);
+}
